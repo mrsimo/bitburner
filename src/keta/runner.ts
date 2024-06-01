1592175dbb202eb83015c6787e44e54d1763ed0b
Albert Llop <mrsimo@gmail.com>
2024-06-01 10:41:03 +0200

    keta: support running against multiple servers

--- src/keta/runner.ts
+++ src/keta/runner.ts
@@ -4,6 +4,7 @@ import Manager from "lib/manager";
 
 import { getRunnableServers } from "lib/explore";
 import { parametersForPercent, KetaParameters } from "keta/calculations";
+import { h4ck } from "lib/break";
 
 // How long we want to give the scripts to land.
 const SlotTime = 2000;
@@ -13,10 +14,24 @@ const SlotTime = 2000;
 //  0ms =>  500ms => 1000ms => 1500ms => 2000ms ...
 const PadTime = SlotTime / 4;
 
+interface KetaParametersDictionary {
+  [index: string]: KetaParameters;
+}
+
 export async function main(ns: NS): Promise<void> {
   ns.disableLog("ALL");
 
-  const target = new Ser(ns, String(ns.args[0]));
+  const options = ns.flags([["target", []]]);
+  let targets;
+  if (options.target instanceof Array) {
+    targets = options.target.map((target) => {
+      h4ck(ns, target);
+      return new Ser(ns, target);
+    });
+  } else {
+    targets = [new Ser(ns, String(options.target))];
+  }
+
   const home = new Ser(ns, "home");
 
   // Kill all lsd scripts that might be running
@@ -28,7 +43,7 @@ export async function main(ns: NS): Promise<void> {
     });
   });
 
-  // Kill previous attemtps for this server
+  // Kill all currently running keta scripts
   ns.getPurchasedServers().forEach((server) => {
     ns.ps(server).forEach((proc) => {
       if (proc.filename.startsWith("keta")) {
@@ -38,7 +53,7 @@ export async function main(ns: NS): Promise<void> {
   });
 
   while (true) {
-    target.reload();
+    targets.forEach((target) => target.reload());
 
     // We're only attempting to run in local servers, they'll be the biggest.
     const servers = ns.getPurchasedServers().map((server) => new Ser(ns, server));
@@ -46,74 +61,79 @@ export async function main(ns: NS): Promise<void> {
     const owner = { ns, home, servers };
 
     const totalMemory = servers.reduce((acc, server) => acc + server.maxRam, 0);
+    const maxMemPerTarget = totalMemory / targets.length;
 
-    let parameters: KetaParameters | undefined;
-    for (let percent = 100; percent >= 1; percent--) {
-      const pars = parametersForPercent(ns, percent, target, PadTime);
-      if (pars.memoryPerSlot < serverMemory) {
-        parameters = pars;
-        break;
+    let parametersPerTarget: KetaParametersDictionary = {};
+    targets.forEach((target) => {
+      for (let percent = 100; percent >= 1; percent--) {
+        const pars = parametersForPercent(ns, percent, target, PadTime);
+        if (pars.memoryPerSlot < maxMemPerTarget) {
+          parametersPerTarget[target.hostname] = pars;
+          break;
+        }
       }
-    }
+    });
 
-    if (!parameters) {
-      ns.tprintf("[keta] %s: [ ] not enough memory to run keta", target.hostname);
-      ns.exit();
-    }
-
-    const maxSlots = Math.floor(parameters.cycleTime / SlotTime) - 1;
-    let slots = Math.min(
-      Math.floor(totalMemory / parameters.memoryPerSlot), // As many slots as they fit in memory
-      maxSlots, // As many slots as they fit in a cycle
+    const shortestCycle = Math.min(
+      ...Object.values(parametersPerTarget).map((parameters) => parameters.cycleTime),
     );
+
+    const maxSlots = Math.floor(shortestCycle / SlotTime) - 1;
     const manager = new Manager(owner);
 
-    ns.tprintf(
-      "[keta] %s: [%s] running %s slots (max %s) @ %s each",
-      target.hostname,
-      target.isPrimed() ? "✔" : " ",
-      slots,
-      maxSlots,
-      parameters.percent + "%",
-    );
+    for (const [hostname, parameters] of Object.entries(parametersPerTarget)) {
+      const target = new Ser(ns, hostname);
+      let slots = Math.min(
+        Math.floor(maxMemPerTarget / parameters.memoryPerSlot), // As many slots as they fit in memory
+        maxSlots, // As many slots as they fit in a cycle
+      );
 
-    ns.tprintf(
-      "[keta] %s: hack: n=%s,t=%s | weaken: n=%s,t=%s | grow: n=%s,t=%s | weaken: n=%s,t=%s",
-      target.hostname,
-      parameters.hackThreads,
-      ns.tFormat(parameters.hackStartTime),
-      parameters.hackWeakenThreads,
-      ns.tFormat(parameters.hackWeakenStartTime),
-      parameters.growThreads,
-      ns.tFormat(parameters.growStartTime),
-      parameters.growWeakenThreads,
-      ns.tFormat(parameters.growWeakenStartTime),
-    );
+      ns.tprintf(
+        "[keta] %s: [%s] running %s slots (max %s) @ %s each",
+        target.hostname,
+        target.isPrimed() ? "✔" : " ",
+        slots,
+        maxSlots,
+        parameters.percent + "%",
+      );
 
-    for (let i = 0; i < slots; i++) {
-      const slotWait = i * SlotTime;
-      manager.ensure("keta/k-hack.js", target.isPrimed() ? parameters.hackThreads : 0, false, [
+      ns.tprintf(
+        "[keta] %s: hack: n=%s,t=%s | weaken: n=%s,t=%s | grow: n=%s,t=%s | weaken: n=%s,t=%s",
         target.hostname,
-        slotWait + parameters.hackStartTime,
-        i,
-      ]);
-      manager.ensure("keta/k-weak1.js", parameters.hackWeakenThreads, false, [
-        target.hostname,
-        slotWait + parameters.hackWeakenStartTime,
-        i,
-      ]);
-      manager.ensure("keta/k-grow.js", parameters.growThreads, false, [
-        target.hostname,
-        slotWait + parameters.growStartTime,
-        i,
-      ]);
-      manager.ensure("keta/k-weak2.js", parameters.growWeakenThreads, false, [
-        target.hostname,
-        slotWait + parameters.growWeakenStartTime,
-        i,
-      ]);
+        parameters.hackThreads,
+        ns.tFormat(parameters.hackStartTime),
+        parameters.hackWeakenThreads,
+        ns.tFormat(parameters.hackWeakenStartTime),
+        parameters.growThreads,
+        ns.tFormat(parameters.growStartTime),
+        parameters.growWeakenThreads,
+        ns.tFormat(parameters.growWeakenStartTime),
+      );
+
+      for (let i = 0; i < slots; i++) {
+        const slotWait = i * SlotTime;
+        manager.ensure("keta/k-hack.js", target.isPrimed() ? parameters.hackThreads : 0, false, [
+          target.hostname,
+          slotWait + parameters.hackStartTime,
+          i,
+        ]);
+        manager.ensure("keta/k-weak1.js", parameters.hackWeakenThreads, false, [
+          target.hostname,
+          slotWait + parameters.hackWeakenStartTime,
+          i,
+        ]);
+        manager.ensure("keta/k-grow.js", parameters.growThreads, false, [
+          target.hostname,
+          slotWait + parameters.growStartTime,
+          i,
+        ]);
+        manager.ensure("keta/k-weak2.js", parameters.growWeakenThreads, false, [
+          target.hostname,
+          slotWait + parameters.growWeakenStartTime,
+          i,
+        ]);
+      }
     }
-
     manager.run();
     await manager.wait();
   }
