ec6eafa30bcad54f69afa4e570b8c2a62c0f4c09
Albert Llop <mrsimo@gmail.com>
2024-06-13 21:52:09 +0200

    Shorter estimateupgrade

--- src/estimateupgrade.ts
+++ src/estimateupgrade.ts
@@ -3,10 +3,10 @@ import { toMoney } from "lib/money";
 
 export async function main(ns: NS): Promise<void> {
   let servers = ns.getPurchasedServers();
-  let ram = 2 * ns.getServerMaxRam(servers[0]);
+  const ram = 2 * Math.min(...servers.map((server) => ns.getServerMaxRam(server)));
 
   ns.tprintf("Estimating upgrade of servers to " + ns.formatRam(ram));
-  const costs = servers.map((server) => ns.getPurchasedServerUpgradeCost(server, ram));
+  const costs = servers.map((server) => Math.max(0, ns.getPurchasedServerUpgradeCost(server, ram)));
   ns.tprintf("Costs: %s", costs.map((cost) => toMoney(cost)).join(", "));
   ns.tprintf("Total: %s", toMoney(costs.reduce((a, b) => a + b, 0)));
 }
